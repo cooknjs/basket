95d556b6e9d6da2c43a43e68c712d8de26e4b529
spadino <spada.andrea@gmail.com>
2021-08-21 20:50:48 -0300

    Add a basic config file

---
+++ cookn.js
@@ -0,0 +1,9 @@
+/* -------------------------------------------------------------------------- */
+/*                            Cook'n'JS main recipe                           */
+/* -------------------------------------------------------------------------- */
+
+module.exports = {
+  title: 'Ready to cook your site?',
+  decription: "Cook'n'JS default starter basket",
+  author: 'spadino @cooknjs'
+}
